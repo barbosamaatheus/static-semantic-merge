957f70fee605c32598cee27ebd1725253242d1ef
M. Barbosa <maatheusb96@gmail.com>
2024-06-04 09:51:34 -0300

    feat: extract args to Arguments class

---
+++ src/main/java/main/Arguments.java
@@ -0,0 +1,39 @@
+package main;
+
+public class Arguments {
+    private String hc;
+    private String[] pc;
+    private String bc;
+    private String dp;
+    private String tpr;
+    private String cn;
+    private String m;
+    private String gp;
+    private String mp;
+    private String sp;
+
+    public Arguments(String hc, String[] pc, String bc, String dp, String tpr, String cn, String m, String gp, String mp, String sp) {
+        this.hc = hc;
+        this.pc = pc;
+        this.bc = bc;
+        this.dp = dp;
+        this.tpr = tpr;
+        this.cn = cn;
+        this.m = m;
+        this.gp = gp;
+        this.mp = mp;
+        this.sp = sp;
+    }
+
+    // Getters
+    public String getHead() { return hc; }
+    public String[] getParents() { return pc; }
+    public String getBase() { return bc; }
+    public String getSsmDependenciesPath() { return dp; }
+    public String getTargetProjectRoot() { return tpr; }
+    public String getClassName() { return cn; }
+    public String getMainMethod() { return m; }
+    public String getGradlePath() { return gp; }
+    public String getScriptsPath() { return sp; }
+    public String getMavenPath() { return mp; }
+}
